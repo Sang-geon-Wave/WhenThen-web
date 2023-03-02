919d1583bb459ca89ef3cf259b2ceee27dd5c529
Sang-geon Yun <45253879+c0510gy@users.noreply.github.com>
2023-03-02 21:58:11 +0900

    init: basic set up for webpack, eslint, and prettier

---
+++ whenthen-web/webpack.config.js
@@ -0,0 +1,60 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: ['babel-polyfill', './src/index.tsx'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    publicPath: '/',
+  },
+  devServer: {
+    historyApiFallback: true,
+    allowedHosts: 'all',
+  },
+  module: {
+    rules: [
+      {
+        // node_modules - only babel, no eslint
+        test: /\.(ts|tsx|js|jsx)$/,
+        include: [path.resolve(__dirname, 'node_modules')],
+        use: ['babel-loader'],
+      },
+      {
+        // project files - babel + eslint
+        test: /\.(ts|tsx|js|jsx)$/,
+        include: [path.resolve(__dirname, './src/')],
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpg|svg|gif)?$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /react-spring/,
+        sideEffects: true,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, 'public/index.html'),
+      filename: 'index.html',
+    }),
+    new ESLintPlugin(),
+  ],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+  },
+};
