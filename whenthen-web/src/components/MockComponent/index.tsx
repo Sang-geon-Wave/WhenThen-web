7c0568b1319871ada8ee5b2788a222dbccd737de
xorud127 <xorud127@kookmin.ac.kr>
2023-03-24 03:36:28 +0900

    add: textedit-compo

--- whenthen-web/src/components/MockComponent/index.tsx
+++ whenthen-web/src/components/MockComponent/index.tsx
@@ -16,7 +16,6 @@ const MockComponent: React.FunctionComponent<ProbsMockComponent> = ({
     screenClass: appStore.screenClass.get(),
   }));
   const isDesktop = screenClass === 'xl';
-
   const styles = isDesktop ? stylesDesktopDefault : stylesDesktopDefault;
 
   return highlight ? (
---
+++ whenthen-web/src/pages/testPage/index.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import useRootData from '../../hooks/useRootData';
+import TextEditomponent from '../../components/TextEditComponent';
+import stylesDesktopDefault from './DesktopDefault.module.scss';
+import stylesMobileDefault from './MobileDefault.module.scss';
+
+const TextPage = () => {
+  return <TextEditomponent />;
+};
+
+export default TextPage;
