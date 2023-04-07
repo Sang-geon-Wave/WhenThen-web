760e4650185d87a4ecc7c0cf92cb4ca21763a673
antifly55 <antifly55@kookmin.ac.kr>
2023-04-07 22:59:45 +0900

    fix: signup page review

--- whenthen-web/src/components/SignupComponent/index.tsx
+++ whenthen-web/src/components/SignupComponent/index.tsx
@@ -4,6 +4,15 @@ import stylesDesktopDefault from './DesktopDefault.module.scss';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
+const enum errMsgTypes {
+  noID = '아이디를 입력해주세요',
+  noPW = '비밀번호를 입력해주세요',
+  noPWRe = '비밀번호 확인을 입력해주세요',
+  existID = '이미 존재하는 ID 입니다',
+  illegalPW = '비밀번호 조건을 만족하지 않습니다',
+  illegalPWRe = '비밀번호 확인이 일치하지 않습니다',
+}
+
 const SignupComponent = () => {
   const { screenClass, isLogin, changeLoginState } = useRootData(
     ({ appStore, loginStore }) => ({
@@ -17,69 +26,74 @@ const SignupComponent = () => {
   const styles = isDesktop ? stylesDesktopDefault : stylesDesktopDefault;
 
   const [signupErrMsg, setSignupErrMsg] = useState('');
-  const [signupErr, setSignupErr] = useState(false);
 
-  const [usrId, setUsrId] = useState('');
+  const [user, setUser] = useState({
+    ID: '',
+    PW: '',
+    PWRe: '',
+  });
+
   const onChangeID = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSignupErr(false);
-    setUsrId(event.currentTarget.value);
+    const updatedUser = { ID: event.currentTarget.value };
+    setUser((user) => ({
+      ...user,
+      ...updatedUser,
+    }));
   };
 
-  const [usrPw, setUsrPw] = useState('');
   const onChangePW = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSignupErr(false);
-    setUsrPw(event.currentTarget.value);
+    const updatedUser = { PW: event.currentTarget.value };
+    setUser((user) => ({
+      ...user,
+      ...updatedUser,
+    }));
   };
 
-  const [usrPwRe, setUsrPwRe] = useState('');
   const onChangePWRe = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSignupErr(false);
-    setUsrPwRe(event.currentTarget.value);
+    const updatedUser = { PWRe: event.currentTarget.value };
+    setUser((user) => ({
+      ...user,
+      ...updatedUser,
+    }));
   };
 
   const submitInfo = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    setSignupErr(false);
+    setSignupErrMsg('');
 
-    if (!usrId) {
-      setSignupErrMsg(`아이디를 입력해주세요`);
-      setSignupErr(true);
+    if (!user.ID) {
+      setSignupErrMsg(errMsgTypes.noID);
       return;
-    } else if (!usrPw) {
-      setSignupErrMsg(`비밀번호를 입력해주세요`);
-      setSignupErr(true);
+    } else if (!user.PW) {
+      setSignupErrMsg(errMsgTypes.noPW);
       return;
-    } else if (!usrPwRe) {
-      setSignupErrMsg(`비밀번호 확인을 입력해주세요`);
-      setSignupErr(true);
+    } else if (!user.PWRe) {
+      setSignupErrMsg(errMsgTypes.noPWRe);
       return;
     }
 
     // id가 이미 있는지 검증 (수정 예정)
-    if (usrId === 'usr') {
-      setSignupErrMsg(`이미 존재하는 ID 입니다`);
-      setSignupErr(true);
+    if (user.ID === 'usr') {
+      setSignupErrMsg(errMsgTypes.existID);
       return;
     }
 
     const reg = /^[A-Za-z0-9]{6,12}$/;
-    if (!reg.test(usrPw)) {
-      setSignupErrMsg('비밀번호 조건이 일치하지 않습니다');
-      setSignupErr(true);
+    if (!reg.test(user.PW)) {
+      setSignupErrMsg(errMsgTypes.illegalPW);
       return;
-    } else if (usrPw !== usrPwRe) {
-      setSignupErrMsg('비밀번호가 일치하지 않습니다');
-      setSignupErr(true);
+    } else if (user.PW !== user.PWRe) {
+      setSignupErrMsg(errMsgTypes.illegalPWRe);
       return;
     }
 
-    alert(`회원가입 완료하였습니다 ${usrId}님`);
+    alert(`회원가입 완료하였습니다 ${user.ID}님`);
   };
 
   return (
     <div className={styles.mainBlock}>
       <Form
-        className={signupErr ? styles.formErrorBlock : styles.formBlock}
+        className={signupErrMsg ? styles.formErrorBlock : styles.formBlock}
         onSubmit={submitInfo}
       >
         <Form.Group className="mb-3" controlId="formBasicId">
@@ -87,7 +101,7 @@ const SignupComponent = () => {
           <Form.Control
             type="text"
             placeholder="아이디"
-            value={usrId}
+            value={user.ID}
             onChange={onChangeID}
           />
         </Form.Group>
@@ -97,7 +111,7 @@ const SignupComponent = () => {
           <Form.Control
             type="text"
             placeholder="비밀번호"
-            value={usrPw}
+            value={user.PW}
             onChange={onChangePW}
           />
         </Form.Group>
@@ -107,10 +121,10 @@ const SignupComponent = () => {
           <Form.Control
             type="password"
             placeholder="비밀번호 확인"
-            value={usrPwRe}
+            value={user.PWRe}
             onChange={onChangePWRe}
           />
-          {signupErr && (
+          {signupErrMsg !== '' && (
             <Form.Text className={styles.errorMessageBlock}>
               {signupErrMsg}
             </Form.Text>
