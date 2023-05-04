1d505e2dd684de97d3692ffa775fba4de6741d8a
SeokHyeon-Eom <eom2244@kookmin.ac.kr>
2023-05-04 22:41:30 +0900

    add: first commit

--- whenthen-web/src/pages/TimelinePage/index.tsx
+++ whenthen-web/src/pages/TimelinePage/index.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import TimelineDateComponent from '../../components/TimelineDateComponent';
 import timeLineMockDataList from '../../assets/strings/TimeLinePage/testData';
-
 import DefaultLayout from '../../layouts/DefaultLayout';
+import api from '../../api';
+import { MovieListByDate } from '../../types/MovieDataType';
+
 const TimelinePage = () => {
-  return (
-    <DefaultLayout>
-      <div>
-        {timeLineMockDataList.map((movieList, idx) => (
-          <TimelineDateComponent movieList={movieList} key={`dateKey${idx}`} />
-        ))}
-      </div>
-    </DefaultLayout>
-  );
+  const getArticle = async () => {
+    const { data } = await api.get('article/like');
+    const userLike = data.users;
+    return userLike;
+  };
+
+  const articleList = getArticle();
+
+  articleList.then((articleList) => {
+    console.log(articleList);
+    return (
+      <DefaultLayout>
+        <div>
+          {articleList.map((articles, idx) => (
+            <TimelineDateComponent movieList={articles} key={`dateKey${idx}`} />
+          ))}
+        </div>
+      </DefaultLayout>
+    );
+  });
 };
 
 export default TimelinePage;
