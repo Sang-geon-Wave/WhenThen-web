6ce83dcf0ea95b1b45806c0efd2abf1e7c4f830e
yhdev7935 <qodydgk8624@kookmin.ac.kr>
2023-05-09 13:23:16 +0900

    feat: connect api server in search component

--- whenthen-web/src/components/SearchComponent/index.tsx
+++ whenthen-web/src/components/SearchComponent/index.tsx
@@ -3,6 +3,7 @@ import InputGroup from 'react-bootstrap/InputGroup';
 import useRootData from '../../hooks/useRootData';
 import stylesDesktopDefault from './DesktopDefault.module.scss';
 import { Button, Dropdown, DropdownButton, Form } from 'react-bootstrap';
+import api from '../../api';
 
 interface PropsSearchComponent {
   types: String[];
@@ -19,9 +20,20 @@ const SearchComponent: React.FunctionComponent<PropsSearchComponent> = ({
   const styles = isDesktop ? stylesDesktopDefault : stylesDesktopDefault;
 
   const [selectedType, setSelectedType] = useState(types[0]);
+  let searchValue = '';
 
   useEffect(() => {}, [selectedType]);
 
+  const getSearchResult = async () => {
+    try {
+      const result = await api.get(
+        `/search?type=${selectedType}&value=${searchValue}`,
+      );
+      console.log(result);
+    } catch (e: any) {
+      console.log(e instanceof Error ? e.message : String(e));
+    }
+  };
   // TODO: connect api server, Form.Control customize(datetime input etc.)
   return (
     <div>
@@ -37,8 +49,15 @@ const SearchComponent: React.FunctionComponent<PropsSearchComponent> = ({
             </Dropdown.Item>
           ))}
         </DropdownButton>
-        <Form.Control aria-label="search" />
-        <Button variant="primary">Search</Button>
+        <Form.Control
+          aria-label="search"
+          onChange={(e) => {
+            searchValue = e.target.value;
+          }}
+        />
+        <Button variant="primary" onClick={getSearchResult}>
+          Search
+        </Button>
       </InputGroup>
     </div>
   );
